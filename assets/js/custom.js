813c0908b9c86bd0dd78128c94a59b7899e50090
Thomas <65050898+tvonment@users.noreply.github.com>
2022-07-15 17:37:03 +0000

    add recaptcha

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -10,3 +10,41 @@ function setMinHeight() {
         content.style.minHeight = window.innerHeight - footer.clientHeight - header.clientHeight + "px"
     }
 }
+
+function onSubmit(token) {
+    document.getElementById("form").submit();
+}
+
+let form = document.getElementById("form");
+if (form) {
+    form.onsubmit = function (event) {
+        event.preventDefault(); // Don't let the browser submit the form.
+        var payload = {};
+
+        // Build JSON key-value pairs using the form fields.
+        form.querySelectorAll("input, textarea").forEach(field => {
+            payload[field.name] = field.value;
+        });
+
+        console.log(payload)
+
+        if (payload["g-recaptcha-response"].length == 0) {
+            recaptchaMessage.style.display = "block";
+        } else {
+            // Post the payload to the contact endpoint.
+            fetch("/api/PostStimme", {
+                method: 'post',
+                body: JSON.stringify(payload)
+            }).then(resp => {
+                if (!resp.ok) {
+                    console.error(resp);
+                    return;
+                }
+                // Display success message.
+                recaptchaMessage.style.display = "none";
+                successMessage.style.display = "block";
+                form.style.display = "none";
+            });
+        }
+    }
+}
--- assets/js/mytheme.js
+++ assets/js/mytheme.js
@@ -2,19 +2,8 @@ window.onscroll = function () { scrollFunction() };
 
 document.addEventListener('DOMContentLoaded', () => {
     setCopyright();
-    setHeaderSpace();
 }, false);
 
 function setCopyright() {
     document.getElementById("copyright").innerText = "© " + new Date().getFullYear() + " " + document.getElementById("copyright").innerText;
-}
-
-function scrollFunction() {
-    if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
-        document.getElementById("header-img").classList.add("header-logo-small");
-        document.getElementById("header-img").classList.remove("header-logo-big");
-    } else {
-        document.getElementById("header-img").classList.add("header-logo-big");
-        document.getElementById("header-img").classList.remove("header-logo-small");
-    }
 }
